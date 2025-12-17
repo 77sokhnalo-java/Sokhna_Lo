917dca70aefcd4bf45b3c0b5249ad6486d0c9db4
77sokhnalo-java <77sokhnalo@gmail.com>
2025-12-17 11:58:55 +0000

    Add files via upload

---
+++ GestionEtudiant.java
@@ -0,0 +1,39 @@
+package gestion;
+import java.util.Scanner;
+
+public class GestionEtudiant {
+	static Etudiant[] etudiants = new Etudiant[50];
+	static int nombreEtudiants = 0;
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		int choix;
+		
+        do {
+            System.out.println("\n===== MENU =====");
+            System.out.println("1. Ajouter un étudiant");
+            System.out.println("2. Trier par âge croissant");
+            System.out.println("3. Afficher les 5 plus âgés et les 5 moyens âgés");
+            System.out.println("4. Supprimer l'étudiant moyen âgé");
+            System.out.println("5. Afficher la meilleure et la plus faible note");
+            System.out.println("0. Quitter");
+            System.out.print("Votre choix : ");
+
+            choix = sc.nextInt();
+
+            switch (choix) {
+                case 1 -> ajouterEtudiant();
+                case 2 -> trierParAge();
+                case 3 -> afficherAges();
+                case 4 -> supprimerMoyenAge();
+                case 5 -> afficherNotes();
+                case 0 -> System.out.println("Fin du programme.");
+                default -> System.out.println("Choix invalide !");
+            }
+        } while (choix != 0);
+    }
+
+}
+
+
