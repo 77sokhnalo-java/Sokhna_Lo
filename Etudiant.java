1943acbd84feb15330173cf8902871c87047376c
77sokhnalo-java <77sokhnalo@gmail.com>
2025-12-17 11:58:17 +0000

    Add files via upload

---
+++ Etudiant.java
@@ -0,0 +1,23 @@
+package gestion;
+
+public class Etudiant {
+	
+	String nom;
+    String prenom;
+    String adresse;
+    String email;
+    String telephone;
+    double note;
+
+    public Etudiant(String nom, String prenom, String adresse, String email, String telephone, double note) {
+                    
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.email = email;
+        this.telephone = telephone;
+        this.note = note;
+    }
+
+
+}
